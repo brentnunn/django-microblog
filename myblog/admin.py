3e9b4ffd41afafafc06bcd230f81ba63a9906cfc
Brent Nunn <brentnunn@yahoo.com>
2015-03-03 09:23:38 -0800

    Inline implemented

--- myblog/admin.py
+++ myblog/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from myblog.models import Post, Category
 
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoriesInLine(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoriesInLine]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
